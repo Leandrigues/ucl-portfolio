234c94be12e601747e9ecb02bde05374ba623921
Leandro Rodrigues <leandro.rodrigues.silva99@gmail.com>
2020-10-31 20:35:33 -0300

    Separate svg in a single-component file

--- src/components/Picker.vue
+++ src/components/Picker.vue
@@ -1,133 +1,6 @@
 <template>
   <div class="picker-container">
-  <svg
-    width="100mm"
-    height="100mm"
-    viewBox="0 0 20 20"
-    >
-    <g
-      inkscape:label="Camada 1"
-      inkscape:groupmode="layer"
-      id="layer1"
-      transform="translate(0,-277)">
-      <rect
-        style="fill:rgb(47,47,47);fill-opacity:1;stroke:none;stroke-width:0.26499999;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-        id="rect6211"
-        width="20"
-        height="20"
-        x="0"
-        y="276.99997"
-        inkscape:export-filename="/home/vitor/Final.png"
-        inkscape:export-xdpi="1136.999"
-        inkscape:export-ydpi="1136.999" />
-      <g
-        transform="matrix(0.1793518,0,0,0.1793518,0.86086854,242.67489)"
-        id="g879-0-0-4"
-        inkscape:export-filename="/home/vitor/Black.png"
-        inkscape:export-xdpi="1067.6328"
-        inkscape:export-ydpi="1067.6328"
-        style="stroke-width:0.89582312">
-        <path
-          style="fill-opacity:1;stroke:none;stroke-width:0.23701985px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-          :style="{ fill: logoSecondColor }"
-
-          d="m 65.982913,244.3391 c 0,-5.90359 -5.905932,-8.75301 -21.066591,0 -15.160659,8.75301 4.769799,11.16176 9.921817,11.31103 5.152018,0.14927 11.144774,-5.40744 11.144774,-11.31103 z"
-          id="path858-2-8-1"
-          inkscape:connector-curvature="0"
-          sodipodi:nodetypes="zzzz" />
-        <path
-          style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.2373931;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
-          :style="{ fill: logoMainColor }"
-          d="m 33.915217,244.3391 v 29.69231 c 0,17.30789 32.067696,17.57182 32.067696,0 V 244.3391 c -6.817536,5.29542 -16.099831,5.09499 -20.982566,0 -4.882735,-5.09499 -11.08513,-11.17194 -11.08513,0 z"
-          id="path856-1-1-0"
-          inkscape:connector-curvature="0"
-          sodipodi:nodetypes="czzczc" />
-        <g
-          id="g889-05-3-4"
-          transform="matrix(0.94855573,0,0,0.99998304,2.6307902,-3.241447)"
-          style="fill:#ffffff;fill-opacity:1;stroke-width:0.89582312"
-          :style="{ fill: logoThirdColor }"
-          >
-          <ellipse
-            ry="4.361074"
-            rx="4.8610744"
-            cy="234.45459"
-            cx="42.397308"
-            id="path862-1-1-8"
-            style="fill-opacity:1;stroke:none;stroke-width:0.24890588;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/>
-        </g>
-        <g
-          transform="matrix(0.82286335,0,0,0.85987993,23.394411,19.500105)"
-          id="g889-5-1-1-7"
-          style="fill-opacity:1;stroke-width:0.89582312"
-          :style="{ fill: logoMainColor }"
-          >
-
-          <ellipse
-            ry="4.361074"
-            rx="4.8610744"
-            cy="234.45459"
-            cx="42.397308"
-            id="path862-6-0-0-0"
-          :style="{ fill: logoSecondColor }"
-            style="fill-opacity:1;stroke:none;stroke-width:0.24890588;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
-        </g>
-        <g
-          transform="matrix(0.61714751,0,0,0.71083408,21.149083,45.853544)"
-          id="g889-0-8-3-8"
-          style="fill-opacity:1;stroke-width:0.89582312"
-          >
-          <ellipse
-            ry="4.361074"
-            rx="4.8610744"
-            cy="234.45459"
-            cx="42.397308"
-            id="path862-9-5-4-62"
-            :style="{ fill: logoSecondColor }"
-            style="fill-opacity:1;stroke:none;stroke-width:0.24890588;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
-          <text
-            xml:space="preserve"
-            style="font-style:normal;font-weight:normal;font-size:15.97878933px;line-height:1.25;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#c8c8c8;fill-opacity:1;stroke:none;stroke-width:0.35785425"
-            x="-11.202062"
-            y="293.86661"
-            id="text989-0-0-4"
-            transform="scale(1.0732222,0.93177354)"><tspan
-              sodipodi:role="line"
-              id="tspan987-6-3-7"
-              x="-11.202062"
-              y="308.00409"
-              style="fill:#c8c8c8;fill-opacity:1;stroke-width:0.35785425" /></text>
-
-        </g>
-        <g
-          aria-label="{"
-          style="font-style:normal;font-weight:normal;font-size:10.58333302px;line-height:1.25;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.23701985;stroke-opacity:1"
-          id="text850-46-9-9"
-          transform="translate(0,-3.1750001)">
-          <path
-            d="m 25.393322,218.89308 v 20.1602 c -1.119195,6.66339 -3.839569,6.84095 -7.880816,9.87879 4.399502,1.1079 7.880816,5.60789 7.880816,12.29747 v 20.1602 c 1.226472,9.74864 12.870421,14.44558 18.738987,10.44432 -5.406599,0 -12.324373,-5.6792 -12.324373,-13.19343 v -20.11888 c 0,-4.94843 -1.924386,-7.88085 -4.948419,-9.43871 3.024033,-1.46617 4.948419,-4.39859 4.948419,-9.43863 v -18.0022 c 0,-4.74547 1.871781,-12.30749 8.905245,-15.48674 -6.797964,-0.0778 -15.201639,4.99715 -15.319859,12.73761 z"
-            style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Cooper Hewitt';-inkscape-font-specification:'Cooper Hewitt';fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:2.05227184;stroke-opacity:1"
-            id="path852-2-1-3"
-            inkscape:connector-curvature="0"
-            sodipodi:nodetypes="cccsccsscsscc" />
-        </g>
-        <g
-          transform="rotate(-180,49.865841,248.51283)"
-          aria-label="{"
-          style="font-style:normal;font-weight:normal;font-size:10.58333302px;line-height:1.25;font-family:sans-serif;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.23701985"
-          id="text850-4-5-9-9" >
-
-          <path
-            d="m 26.451655,221.00975 v 22.27686 c -1.119195,6.66339 -3.839569,6.84095 -7.880816,9.87879 4.399502,1.1079 7.880816,5.60789 7.880816,12.29747 v 15.92687 c -0.0625,6.62797 7.297114,12.03853 14.505654,12.03182 -5.591677,-2.69391 -9.149373,-5.6792 -9.149373,-13.19343 v -17.47305 c 0,-4.94843 -1.924386,-7.88085 -4.948419,-9.43871 3.024033,-1.46617 4.948419,-4.39859 4.948419,-9.43863 v -21.1772 c 0,-7.08137 3.791793,-12.65631 10.341155,-13.59353 -9.208293,-1.98274 -15.618092,4.74542 -15.697436,11.90274 z"
-            style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Cooper Hewitt';-inkscape-font-specification:'Cooper Hewitt';fill:#ffffff;fill-opacity:1;stroke-width:2.05227184"
-            id="path852-0-8-6-2"
-            inkscape:connector-curvature="0"
-            sodipodi:nodetypes="cccsccsscsscc" />
-        </g>
-      </g>
-    </g>
-  </svg>
-
+    <Logo :logoMainColor="logoMainColor" :logoSecondaryColor="logoSecondaryColor" :logoTertiaryColor="logoTertiaryColor"/>
     <div class="color-picker">
       <button @click="setLogoColor('#ff7825', '#ffa46d', '#ff8b46')" class="color-ball orange"></button>
       <button @click="setLogoColor('#5ec8ae', '#99daca', '#75d0ba')" class="color-ball blue"></button>
@@ -138,19 +11,24 @@
 </template>
 
 <script>
+import Logo from './Logo'
+
 export default {
+  components: {
+    Logo
+  },
   data() {
     return {
       logoMainColor: '#ffffff',
-      logoSecondColor: '#c8c8c8',
-      logoThirdColor: '#d4d4d4',
+      logoSecondaryColor: '#c8c8c8',
+      logoTertiaryColor: '#d4d4d4',
     }
   },
   methods: {
-    setLogoColor: function(main, second, third) {
+    setLogoColor: function(main, secondary, tertiary) {
       this.logoMainColor = main
-      this.logoSecondColor = second
-      this.logoThirdColor = third
+      this.logoSecondaryColor = secondary
+      this.logoTertiaryColor = tertiary
       this.$emit('colorPicked')
     }
   }
